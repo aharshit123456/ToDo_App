83fc5a840e9ab5b840e7414d0c8887f784b5551d
Harshit Agarwal <aharshit123456@gmail.com>
2023-12-30 01:16:54 +0530

    Initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color tfRed = Color(0xFFDA4040);
+const Color tfBlue = Color(0xFF5F52EE);
+
+const Color tfBlack = Color(0xFF3A3A3A);
+const Color tfGrey = Color(0xFF717171);
+
+const Color tfBGColor = Color(0xFFEEEFF5);
+
+
